860c826e862a7a74e35e94af701f110b84c3a7c4
Petar Ivanov <peteriv9606@gmail.com>
2021-03-15 22:27:43 +0200

    AddAngularProject

--- src/components/data/projectsData.js
+++ src/components/data/projectsData.js
@@ -1,6 +1,14 @@
 export const projectsData = [
   {
-    id: 0,
+    id: 1,
+    header: "Price Listing Demo",
+    description: "A price listing, responsive demo webpage made using Angular",
+    githubLink: "https://github.com/peteriv9606/angular-app",
+    liveLink: "https://pivanov-angular-build.netlify.app/",
+    imgLink: "../images/angularProject.png",
+  },
+  {
+    id: 1,
     header: "E-Portfolio",
     description:
       "My other e-portfolio website made for a university assignment made using HTML, CSS, JS, jQuery",
@@ -9,7 +17,7 @@ export const projectsData = [
     imgLink: "../images/portfolioResponsive.png",
   },
   {
-    id: 1,
+    id: 2,
     header: "Responsive Webpage Template",
     description:
       "A responsive webpage template made possible using HTML and CSS only.",
@@ -18,7 +26,7 @@ export const projectsData = [
     imgLink: "../images/2create1.png",
   },
   {
-    id: 2,
+    id: 3,
     header: "This website",
     description:
       "My main portfolio website made using React + React-Bootstrap with NodeJS (npm)",
