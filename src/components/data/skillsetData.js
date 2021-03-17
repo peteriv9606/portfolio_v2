63352cd14a7fd3d1a7127b4ddf54541cae5f6c69
Petar Ivanov <peteriv9606@gmail.com>
2021-03-17 12:29:29 +0200

    changes-in-skills

--- src/components/data/skillsetData.js
+++ src/components/data/skillsetData.js
@@ -1,52 +1,43 @@
 export const skillsetData = [
   {
     id: 0,
-    header: "HTML",
-    img: "../images/html.png",
+    header: "Angular",
+    img: "../images/angular.png",
   },
   {
     id: 1,
-    header: "CSS",
-    img: "../images/css.png",
-  },
-  {
-    id: 2,
-    header: "JavaScript",
-    img: "../images/javaScript.png",
-  },
-  {
-    id: 3,
-    header: "Bootstrap",
-    img: "../images/bootStrap.png",
-  },
-  {
-    id: 4,
-    header: "jQuery",
-    img: "../images/jQuery.png",
-  },
-  {
-    id: 5,
     header: "React",
     img: "../images/react.png",
   },
   {
-    id: 6,
-    header: "mySQL",
-    img: "../images/mySQL.png",
+    id: 2,
+    header: "HTML",
+    img: "../images/html.png",
   },
+  {
+    id: 3,
+    header: "CSS",
+    img: "../images/css.png",
+  },
+  {
+    id: 4,
+    header: "JavaScript",
+    img: "../images/javaScript.png",
+  },
+  {
+    id: 5,
+    header: "Bootstrap",
+    img: "../images/bootStrap.png",
+  },
+  {
+    id: 6,
+    header: "jQuery",
+    img: "../images/jQuery.png",
+  },
+
   {
     id: 7,
-    header: "C",
-    img: "../images/c.png",
-  },
-  {
-    id: 8,
-    header: "C++",
-    img: "../images/cpp.png",
-  },
-  {
-    id: 9,
-    header: "C#",
-    img: "../images/csharp.png",
+    header: "mySQL",
+    img: "../images/mySQL.png",
   },
 ];
